091639289cc5265bc051c7e8bd5a5752fc7fa464
Kanishkaj13 <kanishjeyaraja@gmail.com>
2024-03-04 19:26:53 +0530

    minor changes

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const rateLimit = require('express-rate-limit');
 const connectDb = require("./utils/db");
 const dotenv = require("dotenv").config();
 const errorHandler= require ("./middlewares/errorHandler");
-const adminService = require("./services/adminService");
-const userService = require('./services/userService');
+
 
 
 connectDb();
@@ -21,8 +20,7 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT || 300
 app.use(express.json());
 
 app.use("/api/books", require("./routes/bookRoutes"));
