38f2306d183b1b8d3e9bd02f774b95ebab50e90a
Kanishkaj13 <kanishjeyaraja@gmail.com>
2024-03-21 19:29:31 +0530

    nwe commit2

--- server.mjs
+++ server.mjs
@@ -2,19 +2,17 @@ import express, { json } from "express";
 import rateLimit from 'express-rate-limit';
 import connectDb from './config/db.mjs';
 import dotenv from 'dotenv';
-dotenv.config();
-
 import adminRoutes from "./routes/adminRoutes.mjs";
 import assistantLibrarianRoutes from "./routes/assistantLibrarianRoutes.mjs";
 import librarianRoutes from "./routes/librarianRoutes.mjs";
 import accountantRoutes from "./routes/accountantRoutes.mjs";
 import userRoutes from "./routes/userRoutes.mjs";
-
-
 import errorHandler from "./middlewares/errorHandler.mjs";
+import authRoutes from './routes/authRoutes.mjs'
+dotenv.config();
 connectDb();
-const app = express();
 
+const app = express();
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, 
@@ -22,21 +20,20 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again later',
 });
 
+const port = process.env.PORT || 5003;
+
 app.use('/api', limiter);
-
-
-const port = process.env.PORT |5002
 app.use(json());
 
-
-app.use('/admin',adminRoutes);
-app.use('/assistantLibrarian',assistantLibrarianRoutes);
-app.use('/librarian',librarianRoutes);
-app.use('/accountant',accountantRoutes);
-app.use('/user',userRoutes);
+app.use('/auth', authRoutes);
+app.use('/admin', adminRoutes);
+app.use('/assistantLibrarian', assistantLibrarianRoutes);
+app.use('/librarian', librarianRoutes);
+app.use('/accountant', accountantRoutes);
+app.use('/user', userRoutes);
 app.use(errorHandler);
 
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
+  console.log(`Server running on port ${port}`);
+});
