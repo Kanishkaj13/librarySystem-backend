3f477c218d0892ac76b3e259ce326ffc0c8c2c7e
Jagadeep <jagadeep@mantracollab.com>
2024-03-25 16:39:46 +0530

    middlewares changes

--- middlewares/authenticationMiddleware.mjs
+++ middlewares/authenticationMiddleware.mjs
@@ -1,9 +1,9 @@
 // authMiddleware.js
-import bcrypt from  "bcrypt";
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/userModel.mjs";
 
-export const authenticateToken = async (req, res, next) => {
+async function authenticateToken(req, res, next) {
   try {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
@@ -12,7 +12,7 @@ export const authenticateToken = async (req, res, next) => {
       res.status(401).json({ error: "Invalid username or password" });
       return;
     }
-    const validPassword = await bcrypt.compare(password,user.password);
+    const validPassword = await bcrypt.compare(password, user.password);
     console.log(validPassword);
     if (!validPassword) {
       res.status(401).json({ error: "Invalid username or password" });
@@ -20,15 +20,15 @@ export const authenticateToken = async (req, res, next) => {
     }
     console.log(user.username);
     console.log(user.roles);
-    const accessToken = jwt.sign({userName:user.username, roles: user.roles }, process.env.ACCESS_TOKEN_SECERET,
+    const accessToken = jwt.sign({ userName: user.username, roles: user.roles }, process.env.ACCESS_TOKEN_SECERET,
       { expiresIn: "15m" }
     );
     console.log(accessToken);
-    res.status(200).json({accessToken});
-    
-    }
- catch (error) {
-  res.status(401).json({ message: 'Invalid credentials' });
-}
+    res.status(200).json({ accessToken });
+
+  }
+  catch (error) {
+    res.status(401).json({ message: 'Invalid credentials' });
+  }
 };
 export default authenticateToken;
